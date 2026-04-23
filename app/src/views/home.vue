976a81bb4e6cfead56d953239152e65ac57420ea
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2026-04-23 14:02:26 -0400

    work!!!!!

---
+++ app/src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <button>Level 1</button>
+</template>
+
+<script setup>
+// home will be the load screen :)
+// buttons that lead to different levels
+// level1.vue, level2.vue, level3.vue
+// code
+//Get the timing on all the levels
+//Make the function for the user to time it
+//If > the timing, DIEEE
+//If = the timing or has 0.1 second offset, move on and reset it :D
+
+
+</script>
+
+<style scoped>
+button {
+  width: 200px;
+  height: 200px;
+  text-align: center;
+  font-size: 50px;
+}
+</style>
