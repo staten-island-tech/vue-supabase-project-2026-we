38736425774b241d4aeefa1bb1f2367c40b6a8cb
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2026-05-06 14:00:14 -0400

    i hate supabase!!!!!!!!!!!

--- app/src/views/home.vue
+++ app/src/views/home.vue
@@ -42,15 +42,6 @@
 </template>
 
 <script setup>
-// home will be the load screen :)
-// buttons that lead to different levels
-// level1.vue, level2.vue, level3.vue
-// code
-//Get the timing on all the levels
-//Make the function for the user to time it
-//If > the timing, DIEEE
-//If = the timing or has 0.1 second offset, move on and reset it :D
-
 import { ref } from 'vue'
 
 const selectedLevel = ref(null)
