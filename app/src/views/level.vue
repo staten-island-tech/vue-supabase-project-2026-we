b63b5d334eebc430922b6d616c6316ff447996ab
TalonChen <talonc@nycstudents.net>
2026-05-20 13:50:36 -0400

    Update level.vue

--- app/src/views/level.vue
+++ app/src/views/level.vue
@@ -1,7 +1,16 @@
 <template>
-  <controls />
+  <div v-if="level">
+    <Controls />
+  </div>
+  <div v-else>Level not found!</div>
 </template>
 
 <script setup>
-import controls from '@/components/controls.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { getLevelById } from '@/components/levels.js'
+import Controls from '@/components/controls.vue'
+
+const route = useRoute()
+const level = computed(() => getLevelById(route.params.id))
 </script>
